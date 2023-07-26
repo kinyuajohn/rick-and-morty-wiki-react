ef75872c101835d9f942f43d6d916fba301ec3f0
John Kinya <kingjohnkn@gmail.com>
2023-07-26 16:50:19 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(7);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">WiKi</span>
       </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
