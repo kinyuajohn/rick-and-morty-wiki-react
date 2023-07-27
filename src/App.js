67c973162ca6c26661a2709b6a6ab73bd5206f95
John Kinya <kingjohnkn@gmail.com>
2023-07-27 15:55:03 +0300

    search

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
   let [fetchedData, updateFetchedData] = useState([]);
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   useEffect(() => {
     (async function () {
@@ -23,6 +26,9 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">WiKi</span>
       </h1>
+
+      <Search setSearch={setSearch} setPageNumber={setPageNumber} />
+
       <div className="container">
         <div className="row">
           <div className="col-3">
@@ -35,6 +41,7 @@ function App() {
           </div>
         </div>
       </div>
+      <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} />
     </div>
   );
 }
