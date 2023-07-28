ac13160668e323f65ce1be68e58c03da4f712857
John Kinya <kingjohnkn@gmail.com>
2023-07-28 12:24:07 +0300

    filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Gender from "./Category/Gender";
+import Species from "./Category/Species";
+import Status from "./Category/Status";
 
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <div className="col-3">
+      <div className="text-center fw-bold fs-4 mb-2">Filter</div>
+      <div
+        style={{ cursor: "pointer" }}
+        className="text-center text-primary text-decoration-underline"
+      >
+        Clear filters
+      </div>
+      <div className="accordion" id="accordionExample">
+        <Status />
+        <Species />
+        <Gender />
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
